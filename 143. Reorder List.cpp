e047baf7a9ea95ca1f1954d3b07f979243287811
Vamsee Krishna Padala <vamseepadala4@gmail.com>
2025-10-29 15:15:39 +0530

    Added Optimal Solution for 143. Reorder List

--- 143. Reorder List.cpp
+++ 143. Reorder List.cpp
@@ -36,3 +36,55 @@ public:
         
     
 };
+
+//Optimal Solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        
+        ListNode *slow=head,*fast=head->next;
+        while(fast !=nullptr && fast->next!=nullptr)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* curr=slow->next;
+        slow->next=nullptr;
+        ListNode* prev=nullptr;
+
+        while(curr!=nullptr)
+        {
+            ListNode * temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+
+        }
+        ListNode *second =prev;
+
+        ListNode *first = head;
+
+        while (second!=nullptr)
+        {   
+            ListNode *tmp1=first->next;
+            ListNode *tmp2 =second->next;
+            first->next=second;
+            second->next=tmp1;
+            first=tmp1;
+            second=tmp2;
+        }
+    }
+        
+    
+};
